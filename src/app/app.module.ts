5dc23e1633826a0e96d7d57682688198636e76d9
Gilberto Parra <gilbertocarrizo@gmail.com>
2019-01-13 17:34:43 -0400

    animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule,MatButtonModule } from '@angular/material';
+import { HammerCardComponent } from './hammer-card/hammer-card.component';
+// import { HammertimeDirective } from './hammertime.directive';
+
+import * as Hammer from 'hammerjs';
+import { HammerGestureConfig,HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { HammertimeDirective } from './hammertime.directive';
+
+
+export class MyHammerConfig extends HammerGestureConfig{
+  overrides=<any>{
+    'swipe':{ direction: Hammer.DIRECTION_ALL }
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HammerCardComponent,
+    HammertimeDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [{
+    provide:HAMMER_GESTURE_CONFIG,
+    useClass:MyHammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/hammer-card/keyframes.ts
@@ -0,0 +1,16 @@
+import { keyframes,style } from '@angular/animations';
+export const swing=[
+    style({transform:'rotate3d(0,0,1,15deg)',offset:.2}),
+    style({transform:'rotate3d(0,0,1,-10deg)',offset:.4}),
+    style({transform:'rotate3d(0,0,1,5deg)',offset:.6}),
+    style({transform:'rotate3d(0,0,1,-5deg)',offset:.8}),
+    style({transform:'none',offset:1})
+]
+export const wobble=[
+    style({transform:'translate3d(-25%,0,0) rotate3d(0,0,1,-5deg)',offset:.15}),
+    style({transform:'translate3d(20%,0,0) rotate3d(0,0,1,3deg)',offset:.30}),
+    style({transform:'translate3d(-15%,0,0) rotate3d(0,0,1,-3deg)',offset:.45}),
+    style({transform:'translate3d(10%,0,0) rotate3d(0,0,1,2deg)',offset:.60}),
+    style({transform:'translate3d(-5%,0,0) rotate3d(0,0,1,-1deg)',offset:.75}),
+    style({transform:'none',offset:1})
+]
